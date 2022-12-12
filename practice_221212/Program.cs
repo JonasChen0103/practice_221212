e7c12e28981873633296be27cd82bbc3219a0993
Jonas <108212751+JonasChen0103@users.noreply.github.com>
2022-12-12 16:30:37 +0800

    Add project files.

---
+++ practice_221212/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace practice_221212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = (Convert.ToInt32(Console.ReadLine()));
+            times(n, end);
+        }
+        private const int end = 1;
+
+        private static void times(int i, int j)
+        {
+            if(j <= 0 || j >= 10)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine($"{i} * {j} = {i * j}");
+                j++;
+                times(i, j);
+            }
+        }
+    }
+}
